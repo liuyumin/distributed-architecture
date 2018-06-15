f964e5cd0181baec3d58628e9d440c72d6a19d3a
weiwei.wang <weiwei.wang@weimob.com>
2018-06-15 18:45:43 +0800

    co

--- distributed-architecture-server/src/main/java/com/weiwei/distributed/architecture/server/distributedarchitectureserver/DistributedArchitectureServerApplication.java
+++ distributed-architecture-server/src/main/java/com/weiwei/distributed/architecture/server/distributedarchitectureserver/DistributedArchitectureServerApplication.java
@@ -20,6 +20,13 @@ public class DistributedArchitectureServerApplication {
         jettyEmbeddedServletContainerFactory.addServerCustomizers(jettyServerCustomizer);
         jettyEmbeddedServletContainerFactory.setPort(9890);
         jettyEmbeddedServletContainerFactory.setContextPath("/home");
+
+//        http://www.xuxueli.com/xxl-job/#/?id=_11-%E6%A6%82%E8%BF%B0?id=%E3%80%8A%E5%88%86%E5%B8%83%E5%BC%8F%E4%BB%BB%E5%8A%A1%E8%B0%83%E5%BA%A6%E5%B9%B3%E5%8F%B0xxl-job%E3%80%8B
+//
+//
+//        http://www.herohuang.com/2017/06/04/hello-apollo/
+
+
         return jettyEmbeddedServletContainerFactory;
     }
 
